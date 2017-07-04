9a9bc856f27635485863164234d33714cfb0ca30
good edu <goodeducoop@gmail.com>
2017-07-04 23:23:24 +0900

    프레그먼트 연습 2

--- app/src/main/java/com/nawoori/android/fragment/MenuFragment.java
+++ app/src/main/java/com/nawoori/android/fragment/MenuFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class MenuFragment extends Fragment {
@@ -13,6 +14,14 @@ public class MenuFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_menu,container,false);
+
+        Button button = (Button) rootView.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
         return rootView;
     }
